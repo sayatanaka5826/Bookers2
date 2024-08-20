06d595c24e9c749daf3d62217be384b5b508614e
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-08-20 11:04:42 +0000

    [finish]ログイン、ログアウト後の遷移先設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.web_console.allowed_ips = '127.0.0.0/0'
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
