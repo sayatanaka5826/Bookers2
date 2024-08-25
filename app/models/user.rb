19981182a72d11f6fd602ed0fa0bb6f476a2de8d
EC2 Default User <ec2-user@ip-172-31-43-95.ap-northeast-1.compute.internal>
2024-08-25 22:15:18 +0000

    Rspecテスト済

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :image
 
+  validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
